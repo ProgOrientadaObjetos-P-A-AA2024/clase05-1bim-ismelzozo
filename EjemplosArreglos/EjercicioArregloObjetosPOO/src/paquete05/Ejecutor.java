4eab18f7d965bb38f7c8a981e3fbcbfe9c6c621d
david <dizozoranga1@utpl.edu.ec>
2024-05-16 16:19:10 -0500

    clase05

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete05/Ejecutor.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete05/Ejecutor.java
@@ -16,7 +16,8 @@ public class Ejecutor {
         Profesor profesor1 = new Profesor("Tara Hernandez", "contratado");
         Profesor profesor2 = new Profesor("Gregory Walsh", "nombramiento");
         Profesor profesor3 = new Profesor("Kevin Page", "nombramiento");
-        
+        // al establecer profesor c1 y los demas podemos presentar en pantalla 
+        // los datos dados en la clase.
         c1.establecerProfesor(profesor1);
         c2.establecerProfesor(profesor2);
         c3.establecerProfesor(profesor3);
